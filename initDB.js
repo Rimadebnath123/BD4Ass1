6860d2963170264921fbd98d2c0e7a29cb683e96
Rimadebnath123 <rimacse5@gmail.com>
2025-02-12 15:38:54 +0530

    init

--- initDB.js
+++ initDB.js
@@ -1,20 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const path = require('path');
-const dbPath = path.resolve(__dirname, 'database.sqlite');
-
-const db = await open({
-  filename: dbPath,
-  driver: sqlite3.Database,
-});
 // Connect to SQLite database
-// const db = new sqlite3.Database('./database.sqlite', (err) => {
-//   if (err) {
-//     console.error('Error opening database:', err.message);
-//   } else {
-//     console.log('Connected to the SQLite database.');
-//   }
-// });
+const db = new sqlite3.Database('./database.sqlite', (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
 
 db.serialize(() => {
   db.run(
